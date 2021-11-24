3bc8bcfc2dae436bb22f07e231aa3fe7672aaec4
J-Mad-08 <maddox.version1@gmail.com>
2021-11-23 19:34:04 -0500

    first merge

--- src/Components/recoilPOST.js
+++ src/Components/recoilPOST.js
@@ -3,15 +3,15 @@ import { atom, selector } from 'recoil';
 
 // Atoms are units of state(boilerPLATE)
 export const usernameState = atom({
-  key: 'username',
-  default: [],
+	key: 'username',
+	default: [],
 });
 
 // Components can subscribe to selectors just like atoms, and will then be re-rendered when the atoms change
 export const atomData = selector({
-  key: 'postUsername',
-  get: async ({ get }) => {
-    const username = get(usernameState);
-    console.log('username', username);
-  },
+	key: 'postUsername',
+	get: async ({ get }) => {
+		const username = get(usernameState);
+		console.log('username', username);
+	},
 });
