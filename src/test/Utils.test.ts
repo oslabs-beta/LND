c9949e1773234e31e8eafc5b4f7346ab7b233efa
J-Mad-08 <maddox.version1@gmail.com>
2021-12-16 22:33:15 -0500

    remove jest.config.ts to reduce confusion

---
+++ src/test/Utils.test.ts
@@ -0,0 +1,8 @@
+import { Utils } from '../app/Utils';
+
+describe('Utils test suits', () => {
+	test('first test', () => {
+		const abc = Utils.toUpperCase('abc');
+		console.log('It works!');
+	});
+});
